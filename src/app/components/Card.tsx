db0bbffb523f989b9410cb069b2b9d92329ef939
Stanislav_Zinin <onaplain1283@gmail.com>
2024-03-05 20:21:41 +0300

    added navigation button

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,9 +1,13 @@
+import {useNavigate} from "react-router-dom";
+
 type cardType = {
+    id: string
     name: string
     rarity: string
     img: string
 }
 export default function Card(props: cardType) {
+    const navigate = useNavigate()
     return (
         <div style={{
             width: "14rem",
@@ -16,7 +20,9 @@ export default function Card(props: cardType) {
             <img src={props.img} className="card-img-top" alt="Card image" style={{height: '300px'}}/>
             <div style={{display: 'flex', flexDirection: "column", alignItems: 'center'}} className="card-body">
                 <p style={{fontSize: "18px"}}>Rarity:{props.rarity}</p>
-                <button style={{width: "8rem"}} type="button" className="btn btn-primary">Подробнее</button>
+                <button onClick={() => navigate(`/card/:${props.id}`)} style={{width: "8rem"}} type="button"
+                        className="btn btn-primary">Подробнее
+                </button>
             </div>
         </div>
     )
