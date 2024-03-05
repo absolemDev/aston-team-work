0f23ab2ab3f747a5f5570d60507b0d3c1308b2d4
Сасаев Кирилл Вадимович <k.sasaev@makeroi.ru>
2024-03-05 17:10:38 +0300

    Test version

---
+++ src/app/components/pages/main_page/main.tsx
@@ -0,0 +1,40 @@
+// import {
+//   getClassData,
+//   getRaceData,
+//   getFractionData,
+//   getCardSetData,
+// } from "./valuesOfFilter";
+
+// const FilterPage = () => {
+//   const allClasses = getClassData();
+//   const allRaces = getRaceData();
+//   const allFraction = getFractionData();
+//   const allCardSet = getCardSetData();
+
+//   return (
+//     <form>
+//       <select>
+//         {allClasses.map((el) => {
+//           return <option>{el}</option>;
+//         })}
+//       </select>
+//       <select>
+//         {allRaces.map((el) => {
+//           return <option>{el}</option>;
+//         })}
+//       </select>
+//       <select>
+//         {allFraction.map((el) => {
+//           return <option>{el}</option>;
+//         })}
+//       </select>
+//       <select>
+//         {allCardSet.map((el) => {
+//           return <option>{el}</option>;
+//         })}
+//       </select>
+//     </form>
+//   );
+// };
+
+// export { FilterPage };
