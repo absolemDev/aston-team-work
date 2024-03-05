8394cc2484685992be8bd5f19b2a8fd7d7269a36
Stanislav_Zinin <onaplain1283@gmail.com>
2024-03-05 19:38:28 +0300

    Created Card component

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,20 +1,22 @@
 type cardType = {
-    title: string
-    text: string
+    name: string
+    rarity: string
     img: string
 }
 export default function Card(props: cardType) {
     return (
-        <div className="card" style={{width: "14rem", display: 'flex', flexDirection: "column", alignItems: 'center'}}>
-            <img src={props.img} className="card-img-top" alt="..."/>
+        <div style={{
+            width: "14rem",
+            display: 'flex',
+            flexDirection: "column",
+            alignItems: 'center',
+            maxHeight: '500px',
+            padding: '10px'
+        }}>
+            <img src={props.img} className="card-img-top" alt="Card image" style={{height: '300px'}}/>
             <div style={{display: 'flex', flexDirection: "column", alignItems: 'center'}} className="card-body">
-                <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.text}</p>
+                <p style={{fontSize: "18px"}}>Rarity:{props.rarity}</p>
                 <button style={{width: "8rem"}} type="button" className="btn btn-primary">Подробнее</button>
-                <button style={{width: "8rem", marginTop: "1rem"}} type="button" className="btn btn-success">Добавить
-                    в
-                    избранное
-                </button>
             </div>
         </div>
     )
