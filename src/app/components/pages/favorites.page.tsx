b51eaf8eba79153792475873a0d94610444e80c0
Stanislav_Zinin <onaplain1283@gmail.com>
2024-03-11 19:11:40 +0300

    Favourite page

--- src/app/components/pages/favorites.page.tsx
+++ src/app/components/pages/favorites.page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useAppSelector } from "#hooks";
+import { CardMemo } from "../Card";
+import { Container, Row } from "react-bootstrap";
+import React, { useMemo } from "react";
 
-const FavoritesPage = () => {
-  return <h2>Favorites Page</h2>;
+const FavouritesPage = () => {
+  let favourites_ids = useAppSelector((state) => state.user.favorites);
+  let cards = useAppSelector((state) => state.cards.entities);
+  let favourites = useMemo(() => {
+    return cards.filter((el) => favourites_ids.includes(el.cardId));
+  }, [favourites_ids, cards]);
+
+  return (
+    <Container>
+      <h1 className="text-center m-5">Избранное</h1>
+      <Row>
+        {favourites.map((el) => (
+          <CardMemo key={el.cardId} id={el.cardId} img={el.img}></CardMemo>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
-export { FavoritesPage };
+export default FavouritesPage;
