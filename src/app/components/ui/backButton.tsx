35e076bf5468880fd0623c9c3f82611199eb52f0
Ilya Frolov <absolem.dev@gmail.com>
2024-03-12 07:33:45 +0400

    firebase: write data to base

---
+++ src/app/components/ui/backButton.tsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import { ButtonMemo } from "..";
+
+const BackButton = () => {
+  const navigate = useNavigate();
+  return (
+    <ButtonMemo variant="dark" onClick={() => navigate(-1)} className="ms-5">
+      <i className="bi bi-caret-left-fill" />
+      Назад
+    </ButtonMemo>
+  );
+};
+
+export { BackButton };
--- src/app/layouts/default.layout.tsx
+++ src/app/layouts/default.layout.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import {Outlet} from "react-router-dom";
-import {Header} from "../components/ui";
+import { Outlet, useLocation } from "react-router-dom";
+import { Header } from "../components/ui";
+import { BackButton } from "../components/ui/backButton";
 
 const DefaultLayout = () => {
+  const location = useLocation();
+  const showBackButton =
+    location.pathname !== "/" && location.pathname !== "/search";
+
   return (
     <>
-      <Header/>
-      <Outlet/>
+      <Header />
+      {showBackButton && <BackButton />}
+      <Outlet />
     </>
   );
 };
